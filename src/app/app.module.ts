ce4354b323fa203cd8a24f89212c0cfea3e51d07
Olivera Kordic <oliverakordic@Oliveras-MacBook-Pro.local>
2021-09-26 23:36:00 +0200

    Added Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MycomponentComponent } from './mycomponent/mycomponent.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-@NgModule({
+//Angular Material Modules
+import { MatSliderModule } from '@angular/material/slider';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+
+@NgModule({ 
   declarations: [
     AppComponent,
     MycomponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    //Material Modules:
+    MatSliderModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/mycomponent/mycomponent.component.ts
+++ src/app/mycomponent/mycomponent.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-mycomponent',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MycomponentComponent implements OnInit {
 
-  constructor() { }
+  availableOptions: string[] = ['first', 'second', 'third'];
 
-  ngOnInit(): void {
+  myForm: FormGroup = new FormGroup({
+    name: new FormControl('Your name'),
+    selectedOption: new FormControl(this.availableOptions[2]),
+    description: new FormControl('Write your description...'),
+    points: new FormControl(30)
+  });
+
+  constructor(private formBuilder: FormBuilder) { 
+    this.myForm = this.formBuilder.group({
+      name: 'Your name',
+      selectedOption: this.availableOptions[2],
+      description: 'Write your description...',
+      points: 30
+    });
   }
 
+  ngOnInit(): void {
+    // this.myForm = this.formBuilder.group({
+    //   name: 'olja',
+    //   selectedOption: 'third',
+    //   description: 'lalala',
+    //   points: 30
+    // })
+  }
+  
+  onFormSubmit(){
+    console.log('hello! :) Name: '+this.myForm.controls['name'].value+
+    ' Option: '+this.myForm.controls['selectedOption'].value+' Description: '+this.myForm.controls['description'].value+' Points: '+
+    this.myForm.controls['points'].value);
+  }
 }
